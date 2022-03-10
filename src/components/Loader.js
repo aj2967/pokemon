b17196d59a1f9415ac42a78f95468371cc31aee2
aj2967 <aj2967@hotmail.com>
2022-03-10 17:22:02 +0000

    Remove extra files

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,9 @@
-import loader from '../assets/squirtle.svg'
-import rect from '../assets/rect.svg'
 import eclipse from '../assets/eclipse.svg'
 
 const Loader = () => {
   return (
     <div className='loader'>
         <img src={eclipse} alt="Loading..." />
-        {/* <img src={rect} alt="rect" /> */}
-        {/* <object className='loader-svg' data={loader} type="image/svg+xml"></object> */}
     </div>
   )
 }
